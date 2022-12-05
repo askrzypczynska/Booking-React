2483f337820c091c9a8b5d3d672add10f2b8a1ef
askrzypczynska <aleksandraskrzypczynska@gmail.com>
2022-12-05 14:29:16 +0100

    Initial commit

---
+++ src/components/Header/Searchbar/Searchbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Searchbar () {
+    return(
+        <div className="d-flex">
+            <input 
+                className="form-control"
+                type="text" 
+                placeholder='Szukaj...' 
+            />
+            <button className="btn btn-primary">Szukaj</button>
+        </div>
+    )
+}
+
+export default Searchbar;
---
+++ src/components/Hotels/Hotel/Hotel.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Hotel.module.css"
+
+function Hotel(){
+    return (
+        <div className={styles.hotel}>Hotel</div>
+    )
+}
+
+export default Hotel;
