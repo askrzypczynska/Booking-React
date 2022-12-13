384b844f389e605396fabd54449dc7461033f364
askrzypczynska <aleksandraskrzypczynska@gmail.com>
2022-12-13 14:10:21 +0100

    Adding initial functionality for searchbar

--- src/components/Header/Searchbar/Searchbar.js
+++ src/components/Header/Searchbar/Searchbar.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Searchbar () {
+    const [term, setTerm] = useState('');
+    const search = () => {
+        console.log("szukaj", term);
+    }
+    const updateTerm = (e) => {
+        setTerm(e.target.value);
+    }
+    const onKeyDownHandler = (e) =>{
+        if(e.key === 'Enter'){
+            search();
+        }
+    }
+
     return(
         <div className="d-flex">
             <input 
+                value={term}
+                onKeyDown={onKeyDownHandler}
+                onChange={e => setTerm(e.target.value)}
                 className="form-control"
                 type="text" 
                 placeholder='Szukaj...' 
             />
-            <button className="btn btn-primary">Szukaj</button>
+            <button 
+                onClick={search}
+                className="btn btn-primary">Szukaj
+            </button>
         </div>
     )
 }
