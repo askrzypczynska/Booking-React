aa997b0f1fded6d5a19ee56ad5e577c09caae8dd
askrzypczynska <aleksandraskrzypczynska@gmail.com>
2023-01-11 16:41:34 +0100

    adding useReducer to App file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Menu from './components/Menu/Menu';
 import Hotels from './components/Hotels/Hotels';
-import React, { Component } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import LoadingIcon from './components/UI/LoadingIcon/LoadingIcon';
 import Searchbar from './components/UI/Searchbar/Searchbar';
 import Layout from './components/Layout/Layout';
@@ -11,109 +11,123 @@ import ThemeButton from './components/UI/ThemeButton/ThemeButton';
 import ThemeContext from './context/themeContext';
 import AuthContext from './context/authContext';
 
-class App extends Component { 
-  hotels = [
-    {
-      id: 1,
-      name: 'Zabytkowa Świątynia z możliwością noclegu',
-      city: 'Uji - Japonia',
-      rating: 8.8,
-      price: "600 jenów",
-      description: 'Niezależna świątynia buddyjska w Uji. Na terenie obiektu znajdują się dwie małe świątynie: Saishō-in należąca do sekty Tendai-shū i Jōdo-in do Jōdo-shū. Miejsce przechowywania wielu dzieł sztuki i jedna z największych atrakcji turystycznych rejonu Kioto.',
-      image:''
-    },
-    {
-      id: 2,
-      name: '"Fallingwater" Dom',
-      city: 'Mill Run - Stany Zjednoczone',
-      rating: 9.8,
-      price: "1,500 dolarów",
-      description: 'Dom zaprojektowany przez amerykańskiego architekta Franka Lloyda Wrighta w 1935 roku w południowo-zachodniej Pensylwanii, ok. 110 km od Pittsburgha. Dom zbudowany został częściowo nad wodospadem na potoku Bear Run rzeki Youghiogheny. W roku 1963 został przekazany na cele publiczne i przekształcony w muzeum z możliwością wynajmu. Odwiedza go 120 000 gości rocznie.',
-      image:''
-    },
-    {
-      id: 3,
-      name: 'Tradycyjna Chata',
-      city: 'Tengenenge - Zimbabwe',
-      rating: 2.1,
-      price: "25 dolarów",
-      description: 'Tradycyjna chata zbudowana w stylu ludu Tonga, znajdująca się niedaleko Zapory Kariba. Zapora i elektrownia wodna na rzece Zambezi tworzy jezioro Kariba w pasie granicznym pomiędzy Zambią i Zimbabwe. Jest jedną z największych zapór wodnych na świecie.',
-      image:''
-    },
-  ]
-  
-  state = {
-    hotels: [],
-    loading: true,
-    theme: 'dark',
-    isAuthenticated: false,
+const backendHotels = [
+  {
+    id: 1,
+    name: 'Zabytkowa Świątynia z możliwością noclegu',
+    city: 'Uji - Japonia',
+    rating: 8.8,
+    price: "600 jenów",
+    description: 'Niezależna świątynia buddyjska w Uji. Na terenie obiektu znajdują się dwie małe świątynie: Saishō-in należąca do sekty Tendai-shū i Jōdo-in do Jōdo-shū. Miejsce przechowywania wielu dzieł sztuki i jedna z największych atrakcji turystycznych rejonu Kioto.',
+    image:''
+  },
+  {
+    id: 2,
+    name: '"Fallingwater" Dom',
+    city: 'Mill Run - Stany Zjednoczone',
+    rating: 9.8,
+    price: "1,500 dolarów",
+    description: 'Dom zaprojektowany przez amerykańskiego architekta Franka Lloyda Wrighta w 1935 roku w południowo-zachodniej Pensylwanii, ok. 110 km od Pittsburgha. Dom zbudowany został częściowo nad wodospadem na potoku Bear Run rzeki Youghiogheny. W roku 1963 został przekazany na cele publiczne i przekształcony w muzeum z możliwością wynajmu. Odwiedza go 120 000 gości rocznie.',
+    image:''
+  },
+  {
+    id: 3,
+    name: 'Tradycyjna Chata',
+    city: 'Tengenenge - Zimbabwe',
+    rating: 2.1,
+    price: "25 dolarów",
+    description: 'Tradycyjna chata zbudowana w stylu ludu Tonga, znajdująca się niedaleko Zapory Kariba. Zapora i elektrownia wodna na rzece Zambezi tworzy jezioro Kariba w pasie granicznym pomiędzy Zambią i Zimbabwe. Jest jedną z największych zapór wodnych na świecie.',
+    image:''
+  },
+]
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'change-theme':
+      const theme = state.theme === 'dark' ? 'primary' : 'dark'
+      return {...state, theme};
+    case 'set-hotels':
+      return {...state, hotels: action.hotels};
+    case 'set-loading':
+      return {...state, loading: action.loading};
+    case 'login':
+      return {...state, isAuthenticated: true};
+    case 'logout':
+      return {...state, isAuthenticated: false};
+    default:
+      throw new Error('nie ma takiej akcji:' + action.type);
+  }
+}
+
+const initialState = {
+  hotels: [],
+  loading: true,
+  isAuthenticated: false,
+  theme: 'dark'
+}
+
+function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const changeTheme = () => {
+    dispatch({ type: 'change-theme' });
   }
 
-  searchHandler(term) {
-    const hotels = [...this.hotels]
+  const searchHandler = term => {
+    const newHotels = [...backendHotels]
       .filter(x => x.name
         .toLowerCase()
         .includes(term.toLowerCase()));
 
-    this.setState({ hotels });
+    dispatch({ type: 'set-hotels', hotels: newHotels});
   }
 
-  componentDidMount() {
+  useEffect(() => {
     setTimeout(() => {
-      this.setState({
-        hotels: this.hotels,
-        loading: false
-      });
+        dispatch({ type: 'set-hotels', hotels: backendHotels})
+        dispatch({ type: 'set-loading', loading: false})
     }, 1000);
-  }
+  }, []);
 
-  changeTheme = () => {
-    const newTheme = this.state.theme === "primary" ? "dark" : "primary"
-    this.setState({theme: newTheme})
-  }
+  const header = (
+    <Header>
+      <Searchbar 
+      onSearch={term => searchHandler(term)} />
+      <ThemeButton />
+    </Header>
+  );
+  
+  const content = (
+    state.loading ? (
+      <LoadingIcon />
+    ) : (
+      <Hotels hotels={state.hotels}/>)
+  );
 
-  render () {
+  const menu = <Menu />;
+  
+  const footer = <Footer />;
 
-    const header = (
-      <Header>
-        <Searchbar 
-        onSearch={term => this.searchHandler(term)} />
-        <ThemeButton />
-      </Header>
-    );
-    
-    const content = (
-      this.state.loading ? (
-        <LoadingIcon />
-      ) : (
-        <Hotels hotels={this.state.hotels}/>)
-    );
-
-    const menu = <Menu />;
-    
-    const footer = <Footer />;
-
-    return(
-      <AuthContext.Provider value={{ 
-        isAuthenticated: this.state.isAuthenticated,
-        login: () => this.setState({ isAuthenticated: true }),
-        logout: () => this.setState({ isAuthenticated: false }),
+  return(
+    <AuthContext.Provider value={{ 
+      isAuthenticated: state.isAuthenticated,
+      login: () => dispatch({ type: 'login' }),
+      logout: () => dispatch({ type: 'logout' }),
+    }}>
+      <ThemeContext.Provider value={{
+        color: state.theme,
+        changeTheme: () => dispatch({ type: 'change-theme'})
       }}>
-        <ThemeContext.Provider value={{
-          color: this.state.theme,
-          changeTheme: this.changeTheme
-        }}>
-          <Layout 
-            header={header}
-            menu={menu}
-            content={content}
-            footer={footer}
-          />
-        </ThemeContext.Provider>
-      </AuthContext.Provider>
-    );
-  }
-
+        <Layout 
+          header={header}
+          menu={menu}
+          content={content}
+          footer={footer}
+        />
+      </ThemeContext.Provider>
+    </AuthContext.Provider>
+  );
 }
 
 export default App;
